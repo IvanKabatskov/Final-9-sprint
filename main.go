7147a5725095f7c7950f53dc96b481dfb01e8c61
Ivan Kabatskov <Ivan.Kabatskov@atom.team>
2026-04-28 17:32:19 +0300

    develop

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"slices"
+	"sync"
+	"time"
 )
 
 const (
@@ -11,30 +16,81 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size <= 0 || size > 100_000_000 {
+		fmt.Fprintf(os.Stderr, "Некорректная длина слайса: %d. Требуется число от 1 до 100_000_000\n", size)
+		return []int{}
+	}
+	slice := make([]int, size)
+	for i := range slice {
+		slice[i] = rand.Intn(size)
+	}
+	return slice
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
+	if len(data) == 0 {
+		fmt.Fprintf(os.Stderr, "Максимальное число не определено, получен пустой слайс\n")
+		return 0
+	}
+	return slices.Max(data)
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
+	var maxNum []int
+	var wg sync.WaitGroup
+	if len(data)/CHUNKS <= 0 {
+		fmt.Fprintf(os.Stderr, "Слишком короткий слайс, максимально возможное кол-во частей %d\n", len(data))
+		return 0
+	}
+	if len(data)%CHUNKS == 0 {
+		maxNum = make([]int, CHUNKS)
+		counter := 0
+		for chunk := range slices.Chunk(data, len(data)/CHUNKS) {
+			wg.Add(1)
+			go func(i int, chunk []int) {
+				maxNum[i] = slices.Max(chunk)
+				wg.Done()
+			}(counter, chunk)
+			counter++
+		}
+	} else {
+		var length int
+		counter := 0
+		maxNum = make([]int, CHUNKS)
+		for counter < CHUNKS-1 {
+			wg.Add(1)
+			step := length + len(data)/CHUNKS
+			chunk := data[length:step]
+			go func(i int, chunk []int) {
+				maxNum[i] = slices.Max(chunk)
+				wg.Done()
+			}(counter, chunk)
+			counter++
+			length = step
+		}
+		wg.Add(1)
+		chunk := data[length:]
+		go func(i int, chunk []int) {
+			maxNum[i] = slices.Max(chunk)
+			wg.Done()
+		}(counter, chunk)
+	}
+	wg.Wait()
+	return slices.Max(maxNum)
 }
-
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
-
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+	slice := generateRandomElements(SIZE)
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
-
+	start := time.Now()
+	max := maximum(slice)
+	elapsed := time.Since(start).Milliseconds()
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
-
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
-
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	start = time.Now()
+	max = maxChunks(slice)
+	elapsed = time.Since(start).Milliseconds()
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,67 @@
 package main
 
+import "testing"
+
 // Пишите тесты в этом файле
+type testMax []struct {
+	name     string
+	date     []int
+	expected int
+}
+
+func TestGenerateRandomElements(t *testing.T) {
+	t.Run("Стандартный случай", func(t *testing.T) {
+		result := generateRandomElements(100)
+		if len(result) != 100 {
+			t.Errorf("Ожидаемая длина слайса %d != полученной длине слайса %d", 100, len(result))
+		}
+	})
+	t.Run("Диапазон значений", func(t *testing.T) {
+		result := generateRandomElements(10)
+		for _, value := range result {
+			if value < 0 || value > 10 {
+				t.Errorf("Ожидаемые значения в диапазоне от 0 до 9 != полученному значению %d", value)
+			}
+		}
+	})
+	t.Run("Длина слайса 0", func(t *testing.T) {
+		result := generateRandomElements(0)
+		if len(result) != 0 {
+			t.Errorf("Ожидаемая длина слайса 0 != полученному значению %d", len(result))
+		}
+	})
+	t.Run("Слишком большой размер", func(t *testing.T) {
+		result := generateRandomElements(100_000_001)
+		if len(result) != 0 {
+			t.Errorf("Ожидаемая длина слайса 0 != полученному значению %d", len(result))
+		}
+	})
+}
+
+func TestMaximum(t *testing.T) {
+	tests := testMax{
+		{
+			name:     "Стандартный случай",
+			date:     []int{1, 100, 200, 500, 0},
+			expected: 500,
+		},
+		{
+			name:     "Пустой слайс",
+			date:     []int{},
+			expected: 0,
+		},
+		{
+			name:     "Слайс с одним элементом",
+			date:     []int{100},
+			expected: 100,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := maximum(test.date)
+			if result != test.expected {
+				t.Errorf("Получено %d != ожидалось %d", result, test.expected)
+			}
+		})
+	}
+}
